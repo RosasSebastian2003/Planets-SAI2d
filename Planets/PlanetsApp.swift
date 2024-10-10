487da25aafb7aec02269d8d4ed2c68e3b80c0b70
Sebastian Rosas <rosassebastian2003@gmail.com>
2024-10-10 13:22:08 -0600

    initial commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Sebastian Rosas Maciel on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
